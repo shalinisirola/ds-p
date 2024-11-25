f8646270fc2ea77e1a3b15ffc0d20eabd451a758
Shalini Singh Sirola <159011436+shalinisirola@users.noreply.github.com>
2024-11-25 10:12:47 +0530

    Update p10.c

--- p10.c
+++ p10.c
@@ -39,4 +39,32 @@ for(i=0; i<=10; i++)
 printf("a[%d] === >> %d\n", i, a[i]);
 
 }
+/*
+Enter the number ( < 05) of elements to store
+ 5
+ Enter the m value, h(k) = k mod m
+ 11
+ Enter the key value to be stored
+ 34
+ Enter the key value to be stored
+ 36
+ Enter the key value to be stored
+ 79
+ Enter the key value to be stored
+ 81
+ Enter the key value to be stored
+ 56
+ The keys in Hashed location are
+ a[0] === >> -999
+ a[1] === >> 34
+ a[2] === >> 79
+ a[3] === >> 36
+ a[4] === >> 81
+ a[5] === >> 56
+ a[6] === >> -999
+ a[7] === >> -999
+ a[8] === >> -999
+ a[9] === >> -999
+ a[10] === >> -999
+*/
 
