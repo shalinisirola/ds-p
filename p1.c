d16938af386c52fe930582647f3cd0258704b7fb
Shalini Singh Sirola <159011436+shalinisirola@users.noreply.github.com>
2025-01-27 14:39:02 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,141 +1,120 @@
 #define Sz 5
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
-void push(int S[Sz], int *top, int item){
-	if(*top==Sz-1){
-		printf ("Stack Overflow \n"); 
-		return;
-	}
-	*top= *top+1;
-	S[*top]=item;
-	printf("%d inserted into the stack\n", item);
-}
-
-void pop (int S[Sz], int *top)
-{
-    if(*top==-1)
-    {
-		printf("Stack underflow \n"); 
-		return;
-	}
-	printf("%d is deleted \n", S[*top]);
-    *top=*top-1;
-}
-
-void display (int S[Sz], int top){
-	int i;
-	if (top==-1){
-		printf ("Empty Stack In");
-		return;
-	}
-	printf("Content of Stack \n");
-	for(i=0; i<=top; i++){
-		printf("%d \n",S[i]);
-	}
-}
-
-int main(){
-	
-    int S[Sz], top=-1, item, ch;
-    for(;;){
-		printf("1:Insert 2: Delete 3: Display 4:Exit \n");
-		scanf("%d", &ch);
-		switch (ch){
-			case 1: printf("Enter the item \n");
-			        scanf("%d", & item);
-			        push (S, &top, item);
-			        break;
-            
-            case 2: pop(S, &top); 
-                    break;
-                    
-            case 3: display (S, top);
-                    break;
-            
-            case 4: exit (0);
-            
-            default: printf("Invalid Choice, Please try again");
-		}
-	}
-	return 0;
-}
-
-
-/*
-
-
-#include "stdio.h"
-#include "stdlib.h"
-#define SIZE 6
-
-int push(int* arr, int* top, int value){
-    if(*top>=SIZE-1){
-        printf("Cannot push. Stack overflow\n");
+// Function to insert an element in the stack
+void push(int S[Sz], int *top, int item) {
+    if (*top == Sz - 1) {
+        printf("Stack Overflow\n");
         return;
     }
-    arr[++(*top)]=value;
+    *top = *top + 1;
+    S[*top] = item;
+    printf("%d inserted into the stack\n", item);
 }
 
-int pop(int* arr, int* top){
-    if(*top<0){
-        printf("Stack underflow\n");
-        return NULL;
-    }
-
-    int temp = arr[(*top)--];
-    printf("Popped : %d\n", temp);
-    return arr[*top];
-
-}
-
-void display(int* arr, int top){
-    if(top<0){
-        printf("Stack is empty\n");
+// Function to delete an element from the stack
+void pop(int S[Sz], int *top) {
+    if (*top == -1) {
+        printf("Stack Underflow\n");
         return;
     }
-    printf("Stack is : \n");
-    for(int i=0;i<=top;i++){
-        printf("%d ", arr[i]);
-    }  
-    printf("\n");
+    printf("%d is deleted\n", S[*top]);
+    *top = *top - 1;
 }
 
-int main()
-{
-    int arr[SIZE];
-    int top = -1;
+// Function to display the stack contents
+void display(int S[Sz], int top) {
+    int i;
+    if (top == -1) {
+        printf("Stack is Empty\n");
+        return;
+    }
+    printf("Content of Stack:\n");
+    for (i = 0; i <= top; i++) {
+        printf("%d\n", S[i]);
+    }
+}
 
-    int value, ch;
-    printf("1) Push 2) Pop 3) Display 4) Exit\n");
+// Function to check for palindrome
+void palindrome(int S[Sz], int top) {
+    if (top == -1) {
+        printf("Stack is Empty\n");
+        return;
+    }
 
-    while(1){
-        printf("Enter choice : ");
-        scanf("%d", &ch);
-        switch (ch)
-        {
-        case 1:
-            printf("Enter element to insert : ");
-            scanf("%d", &value);
-            push(arr, &top, value);
-            break;
-        case 2:
-            pop(arr, &top);
-            break;
-        case 3:
-            display(arr, top);
-            break;
-        
-        default:
-            printf("Exiting..");
-            exit(0);
+    int rev[Sz], len = top + 1;
+    int i, isPalindrome = 1;
+
+    // Reverse the array
+    for (i = 0; i < len; i++) {
+        rev[i] = S[top - i];
+    }
+
+    // Display the original and reversed array
+    printf("Array elements are:\n");
+    for (i = 0; i < len; i++) {
+        printf("%d\t", S[i]);
+    }
+    printf("\nReverse array:\n");
+    for (i = 0; i < len; i++) {
+        printf("%d\t", rev[i]);
+    }
+
+    // Check for palindrome
+    for (i = 0; i < len; i++) {
+        if (S[i] != rev[i]) {
+            isPalindrome = 0;
             break;
         }
     }
 
+    if (isPalindrome) {
+        printf("\nIt is a palindrome number\n");
+    } else {
+        printf("\nIt is not a palindrome number\n");
+    }
 }
-*/
+
+// Main function
+int main() {
+    int S[Sz], top = -1, item, ch;
+
+    for (;;) {
+        printf("1: Insert 2: Delete 3: Display 4: Palindrome 5: Exit\n");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                printf("Enter the item:\n");
+                scanf("%d", &item);
+                push(S, &top, item);
+                break;
+
+            case 2:
+                pop(S, &top);
+                break;
+
+            case 3:
+                display(S, top);
+                break;
+
+            case 4:
+                palindrome(S, top);
+                break;
+
+            case 5:
+                exit(0);
+
+            default:
+                printf("Invalid Choice, Please try again\n");
+        }
+    }
+
+    return 0;
+}
+
 
 
 
